0972fb1dd297b7410e2bdad24f92b6f10ffe75b3
Anissa Molina <ani_luz15@yahoo.com>
2024-02-20 09:23:45 -0800

    excersise 3

--- 3-everything.js
+++ 3-everything.js
@@ -1,10 +1,29 @@
 function every(array, test) {
-    
+  for (let element of array) {
+    if (!test(element) === true) {
+      return false
+    }
   }
-  
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
+  return true;
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+
+
+function every(array, test) {
+    return !array.some(element => !test(element));
+  }
+
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
